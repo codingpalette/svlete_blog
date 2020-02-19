32049aca7523bf8b2f8586db2464e4b4468b518b
이성재 <adfffewr@naver.com>
2020-02-19 11:47:46 +0900

    firebase 설치

--- src/client.js
+++ src/client.js
@@ -1,5 +1,21 @@
 import * as sapper from '@sapper/app';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-sapper.start({
-	target: document.querySelector('#sapper')
-});
+sapper
+  .start({
+    target: document.querySelector('#sapper')
+  })
+  .then(() => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyA7-pMLxi8lpNQ21ny1e4-DAFxaJYqmMeY',
+      authDomain: 'my-blog-7d61e.firebaseapp.com',
+      databaseURL: 'https://my-blog-7d61e.firebaseio.com',
+      projectId: 'my-blog-7d61e',
+      storageBucket: 'my-blog-7d61e.appspot.com',
+      messagingSenderId: '848807520356',
+      appId: '1:848807520356:web:58361cf8c464c5452f1771',
+      measurementId: 'G-M13M8NZS5Z'
+    };
+    firebase.initializeApp(firebaseConfig);
+  });
