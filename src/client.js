0c96d0caa5c782aa0e275dcf40a175469f770364
이성재 <adfffewr@naver.com>
2020-02-20 14:08:36 +0900

    firebase key 제거

--- src/client.js
+++ src/client.js
@@ -8,14 +8,5 @@ sapper.start({
   target: document.querySelector('#sapper')
 });
 
-let firebaseConfig = {
-  apiKey: 'AIzaSyA7-pMLxi8lpNQ21ny1e4-DAFxaJYqmMeY',
-  authDomain: 'my-blog-7d61e.firebaseapp.com',
-  databaseURL: 'https://my-blog-7d61e.firebaseio.com',
-  projectId: 'my-blog-7d61e',
-  storageBucket: 'my-blog-7d61e.appspot.com',
-  messagingSenderId: '848807520356',
-  appId: '1:848807520356:web:58361cf8c464c5452f1771',
-  measurementId: 'G-M13M8NZS5Z'
-};
+let firebaseConfig = {};
 firebase.initializeApp(firebaseConfig);
