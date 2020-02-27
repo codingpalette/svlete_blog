c0cd3c6c10b3f6cd959961c265a2aebeea7a6de5
이성재 <adfffewr@naver.com>
2020-02-27 13:59:57 +0900

    유저 정보 테이블 작업

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -1,7 +1,11 @@
 <script>
   import Header from "../../components/Header.svelte";
-  import Welcome from "../../components/Welcome.svelte";
   import { test, user } from "../../api";
+
+  import Container from "sveltestrap/src/Container.svelte";
+  import Table from "sveltestrap/src/Table.svelte";
+
+  let items = [];
 
   const click = async () => {
     try {
@@ -16,6 +20,7 @@
     try {
       const res = await user.Get();
       console.log(res);
+      items = res.data.items;
     } catch (e) {
       console.log(e.message);
     }
@@ -27,7 +32,27 @@
 <div class="main_container">
   <button on:click={click}>api 테스트</button>
   <button on:click={userClick}>유저 테스트</button>
-  <Welcome>
-    <h1 slot="title">어드민 페이지 준비중입니다.</h1>
-  </Welcome>
+  <Container>
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>이메일</th>
+          <th>아이디</th>
+          <th>사진</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each items as { email, uid, photoURL }, i}
+          <tr>
+            <th scope="row">{i + 1}</th>
+            <td>{email}</td>
+            <td>{uid}</td>
+            <td>{photoURL}</td>
+          </tr>
+        {/each}
+
+      </tbody>
+    </Table>
+  </Container>
 </div>
