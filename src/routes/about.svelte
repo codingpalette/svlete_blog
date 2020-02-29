3dc3b0f9b3657b0040cb7e319efc6aab8a5065a6
이성재 <adfffewr@naver.com>
2020-02-29 12:40:30 +0900

    어바웃 페이지 수정

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -29,6 +29,21 @@
     border-left: 4px solid #ccc;
     text-indent: 1rem;
     margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+  .list_container h2 {
+    font-size: 1.5rem;
+    color: #000;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 0;
+  }
+  .list_container li {
+    list-style-type: disc;
+    list-style-position: inside;
+  }
+  .list_container li + li {
+    padding-top: 0.5rem;
   }
 </style>
 
@@ -46,6 +61,15 @@
         <img src="image/profile.svg" alt="profile" />
       </div>
       <strong>좋은 개발자를 꿈꾸는 블로그 입니다.</strong>
+      <div class="list_container">
+        <h2>소개</h2>
+        <ul class="list_box">
+          <li>이름 : 이성재</li>
+          <li>취미 : 코딩, 게임</li>
+          <li>직업 : Frontend 개발자</li>
+        </ul>
+      </div>
+
     </div>
   </Container>
   <!-- <Welcome>
